f5f7ed2e0a662aaf832435bb1aecaee039045999
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-07-12 14:09:28 +0700

    add capability to download image from URL with header

--- QAsyncImageView/QAsyncImageView/QAsyncImageView.swift
+++ QAsyncImageView/QAsyncImageView/QAsyncImageView.swift
@@ -14,11 +14,11 @@ public class QAsyncImageView: UIImageView {
 
 }
 public extension UIImageView {
-    public func loadAsync(url:String, placeholderImage:UIImage? = nil){
+    public func loadAsync(url:String, placeholderImage:UIImage? = nil, header : [String : String] = [String : String]()){
         if placeholderImage != nil {
             self.image = placeholderImage!
         }
-        imageForUrl(url, completionHandler:{(image: UIImage?, url: String) in
+        imageForUrl(url, header: header, completionHandler:{(image: UIImage?, url: String) in
             self.image = image
         })
     }
@@ -31,7 +31,7 @@ public extension UIImageView {
     //  git: https://github.com/natelyman/SwiftImageLoader
     //  Copyright (c) 2014 NateLyman.com. All rights reserved.
     //
-    func imageForUrl(urlString: String, completionHandler:(image: UIImage?, url: String) -> ()) {
+    func imageForUrl(urlString: String, header: [String : String] = [String : String](), completionHandler:(image: UIImage?, url: String) -> ()) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {()in
             let cache = NSCache()
             let data: NSData? = cache.objectForKey(urlString) as? NSData
@@ -43,24 +43,50 @@ public extension UIImageView {
                 })
                 return
             }
-            
-            let downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: urlString)!, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-                if (error != nil) {
-                    completionHandler(image: nil, url: urlString)
-                    return
+            if header.count > 0 {
+                let url = NSURL(string: urlString)
+                let mutableRequest = NSMutableURLRequest(URL: url!)
+                
+                for (key, value) in header {
+                    mutableRequest.setValue(key, forHTTPHeaderField: value)
                 }
                 
-                if let data = data {
-                    let image = UIImage(data: data)
-                    cache.setObject(data, forKey: urlString)
-                    dispatch_async(dispatch_get_main_queue(), {() in
-                        completionHandler(image: image, url: urlString)
-                    })
-                    return
-                }
-                
-            })
-            downloadTask.resume()
+                let downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithRequest(mutableRequest, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                    if (error != nil) {
+                        completionHandler(image: nil, url: urlString)
+                        return
+                    }
+                    
+                    if let data = data {
+                        let image = UIImage(data: data)
+                        cache.setObject(data, forKey: urlString)
+                        dispatch_async(dispatch_get_main_queue(), {() in
+                            completionHandler(image: image, url: urlString)
+                        })
+                        return
+                    }
+                    
+                })
+                downloadTask.resume()
+            }else{
+                let downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: urlString)!, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                    if (error != nil) {
+                        completionHandler(image: nil, url: urlString)
+                        return
+                    }
+                    
+                    if let data = data {
+                        let image = UIImage(data: data)
+                        cache.setObject(data, forKey: urlString)
+                        dispatch_async(dispatch_get_main_queue(), {() in
+                            completionHandler(image: image, url: urlString)
+                        })
+                        return
+                    }
+                    
+                })
+                downloadTask.resume()
+            }
         })
         
     }
