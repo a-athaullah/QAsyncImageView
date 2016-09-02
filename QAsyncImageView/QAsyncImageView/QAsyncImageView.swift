10047c5885ec0c73b2ae43e56e26473cf8a088ac
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-09-02 10:04:23 +0700

    add masking option on loadAsync

--- QAsyncImageView/QAsyncImageView/QAsyncImageView.swift
+++ QAsyncImageView/QAsyncImageView/QAsyncImageView.swift
@@ -12,21 +12,54 @@ import Foundation
 var cache = NSCache()
 
 public class QAsyncImageView: UIImageView {
-
-
+    
+    
 }
 public extension UIImageView {
-    public func loadAsync(url:String, placeholderImage:UIImage? = nil, header : [String : String] = [String : String](), useCache:Bool = true){
+    public func loadAsync(url:String, placeholderImage:UIImage? = nil, header : [String : String] = [String : String](), useCache:Bool = true, maskImage:UIImage? = nil){
+        var returnImage = UIImage()
         if placeholderImage != nil {
-            self.image = placeholderImage!
+            if maskImage != nil{
+                returnImage = UIImageView.maskImage(placeholderImage!, mask: maskImage!)
+            }else{
+                returnImage = placeholderImage!
+            }
+            self.image = returnImage
+            
         }else{
             self.image = nil
         }
         imageForUrl(url, header: header, useCache: useCache, completionHandler:{(image: UIImage?, url: String) in
-                if image != nil {
-                    self.image = image
+            if image != nil {
+                if maskImage != nil{
+                    print("here")
+                    returnImage = UIImageView.maskImage(image!, mask: maskImage!)
+                }else{
+                    returnImage = image!
                 }
-            })
+                self.image = returnImage
+            }
+        })
+    }
+    
+    public class func maskImage(image:UIImage, mask:(UIImage))->UIImage{
+        
+        let scaledImage = UIImage.resizeImage(image, toFillOnImage: mask)
+        let imageReference = scaledImage.CGImage
+        let maskReference = mask.CGImage
+        
+        let imageMask = CGImageMaskCreate(CGImageGetWidth(maskReference),
+                                          CGImageGetHeight(maskReference),
+                                          CGImageGetBitsPerComponent(maskReference),
+                                          CGImageGetBitsPerPixel(maskReference),
+                                          CGImageGetBytesPerRow(maskReference),
+                                          CGImageGetDataProvider(maskReference), nil, false)
+        
+        let maskedReference = CGImageCreateWithMask(imageReference, imageMask)
+        
+        let maskedImage = UIImage(CGImage:maskedReference!)
+        
+        return maskedImage
     }
     
     // func imageForUrl
@@ -92,4 +125,46 @@ public extension UIImage {
     public class func clearCachedImageForURL(urlString:String){
         cache.removeObjectForKey(urlString)
     }
+    public class func resizeImage(image: UIImage, toFillOnImage: UIImage) -> UIImage {
+        
+        var scale:CGFloat = 1
+        var newSize:CGSize = toFillOnImage.size
+        
+        if image.size.width > image.size.height{
+            scale = image.size.width / image.size.height
+            newSize.width = toFillOnImage.size.width
+            newSize.height = toFillOnImage.size.height / scale
+        }else{
+            scale = image.size.height / image.size.width
+            newSize.height = toFillOnImage.size.height
+            newSize.width = toFillOnImage.size.width / scale
+        }
+        
+        var scaleFactor = newSize.width / image.size.width
+        
+        
+        if (image.size.height * scaleFactor) < toFillOnImage.size.height{
+            scaleFactor = scaleFactor * (toFillOnImage.size.height / (image.size.height * scaleFactor))
+        }
+        if (image.size.width * scaleFactor) < toFillOnImage.size.width{
+            scaleFactor = scaleFactor * (toFillOnImage.size.width / (image.size.width * scaleFactor))
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(toFillOnImage.size, false, scaleFactor)
+        
+        var xPos:CGFloat = 0
+        if (image.size.width * scaleFactor) > toFillOnImage.size.width {
+            xPos = ((image.size.width * scaleFactor) - toFillOnImage.size.width) / 2
+        }
+        var yPos:CGFloat = 0
+        if (image.size.height * scale) > toFillOnImage.size.height{
+            yPos = ((image.size.height * scaleFactor) - toFillOnImage.size.height) / 2
+        }
+        image.drawInRect(CGRectMake(0 - xPos,0 - yPos, image.size.width * scaleFactor, image.size.height * scaleFactor))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
 }
